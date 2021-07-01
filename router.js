a87de2f0f25ba96585bdd01a028c01d4571ff68b
Nathan <contact@mindbodyeducation.com>
2021-07-01 10:44:59 +1000

    getting rid of some uneeded code

--- router.js
+++ router.js
@@ -3,77 +3,7 @@ const express = require("express"),
 const usersController = require("./controllers/user_controller");
 const profileController = require("./controllers/profile_controller");
 
-// const inventoryController = require("./controllers/inventory_controller");
-// const postController = require("./controllers/post_controller");
 
-// request.connection.remoteAddress IP ADDRESS request.headers['x-forwarded-for'] 
-// var Scraper = require("image-scraper");
- 
-// var scraper = new Scraper("https://apod.nasa.gov/apod/astropix.html");
-// scraper.address = 'https://www.prikol.ru/wp-content/gallery/october-2010/';
-
-
-// let scrape = async (req, res, next) => {
-//   req.DATA = []
-//   try{
-//     await scraper.scrape( (image) => { 
-//       // image.save();
-//       // console.log("new image")
-//       if(image.name.indexOf("image2") == -1)
-//         req.DATA.push(image)
-//     })
-//   }catch(e) {
-//     console.log(e);
-//   }
-
-//   let timer = setInterval( () => next(), 5000)
-//   // timer()
-// }
-
-const rp = require('request-promise');
-const $ = require('cheerio');
-const url = '';
-
-
-
-router.get("/viewimage/:start/:end",  async (req, res) => {
-  // console.log(req.DATA)
-  // rp(url)
-  // .then(function(html){
-  //   //success!
-  //   console.log($('td > a', html).length);
-  //   console.log($('td > a', html));
-  // })
-  const start = parseInt(req.params.start)
-  const end = parseInt(req.params.end)
-  const wikiUrls = [];
-  rp(url)
-  .then(function(html){
-    //success!
-    // for (let i = start; i < end; i++) {
-    let i = start
-    while(wikiUrls.length < end){
-      let link = $('td > a', html)[i]
-      
-      if(link != undefined && 'attribs' in link){
-        link = $('td > a', html)[i].attribs.href
-        if(link.toLowerCase().indexOf("") != -1){ 
-          wikiUrls.push(url + link);
-        }
-      }
-      i++;
-    }
-    console.log(wikiUrls);
-  }).then( () => {
-    res.render('img-viewer', {data: wikiUrls})
-
-  })
-  .catch(function(err){
-    console.log(err);
-    //handle error
-  });
-  // res.send("loaded")
-});
 router.get("/inventory/:uuid", usersController.checkIP, usersController.loadInventory);
 router.get("/profile/inventory/:uuid", profileController.loadInventory);
 router.get("/profile/perks/:uuid", profileController.loadPerks);
